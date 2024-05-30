08ab894d59686e69560c6d2e97fbef6e3dfe5ae8
akarpovskii <an.karpovskii@gmail.com>
2024-05-30 20:41:55 +0400

    Fix build for zig master

--- build.zig
+++ build.zig
@@ -7,7 +7,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     _ = b.addModule("build.crab", .{
-        .root_source_file = .{ .path = "src/root.zig" },
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
